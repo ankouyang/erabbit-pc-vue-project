1b4aea06baf9836e81eaf93623f43673d7d9da95
杨科 <yangke@xinpayroll.com>
2022-10-26 19:22:53 +0800

    jsconfig.json文件移动

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
     const store = useStore()
     console.log(store.state.userInfo.userName)
     console.log(store.getters.getUserName)
-    // 可直接调用，无需区分模块
+    // 可直接调用，无需区分模块    
     store.commit('UPDATE_NAME', '2323')
     store.dispatch('updateName')
 
